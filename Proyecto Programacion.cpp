4d96e7ecc6523edfb54606ffe9118ac4bd21530a
Williamf40 <williamf4mendoza20@gmail.com>
2024-11-21 08:17:15 -0500

    proyecto william

---
+++ Proyecto Programacion.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main (){
+	cout<<"El jugador encarna a un heroe que recorre un calabozo enfrentándose"<<endl;
+	cout<<"a una serie de enemigos y tomando decisiones estrategicas para sobrevivir,"<<endl;
+	cout<<"dentro de este mundo las preguntas juegan un papel fundamental buscando determinar"<<endl;
+	cout<<"el valor o la decadencia del heroe. Durante el juego el jugador tendra que"<<endl;
+	cout<<"decidir si luchar o huir, y ganara puntos o perdera vida dependiendo de"<<endl; 
+	cout<<"sus decisiones y de los resultados de los enfrentamientos"<<endl;
+	return 0;
+}
