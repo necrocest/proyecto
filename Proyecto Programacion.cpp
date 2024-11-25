e6d304dcfe23468a75a7be56d2c5f1282ceb5b28
Williamf40 <63164108+necrocest@users.noreply.github.com>
2024-11-24 23:40:13 -0500

    Update Proyecto Programacion.cpp

--- Proyecto Programacion.cpp
+++ Proyecto Programacion.cpp
@@ -1,32 +1,26 @@
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
 using namespace std;
 // Estructura para las estadísticas de un personaje
-struct Estadisticas {
+struct Estadisticas{
     int vida;
     int ataque;
     int defensa;
-};
-// Prototipos de funciones
+}
 // Función para mostrar estadísticas
-void mostrarEstadisticas(const Estadisticas& jugador, const Estadisticas& enemigo, int nivel);
-void mostrarEstadisticas(const Estadisticas& jugador, const Estadisticas& enemigo, int nivel) {
-    cout << "Jugador - Vida: " << jugador.vida << ", Ataque: " << jugador.ataque << ", Defensa: " << jugador.defensa << ", Nivel: " << nivel << "\n";
-    cout << "Enemigo - Vida: " << enemigo.vida << ", Ataque: " << enemigo.ataque << ", Defensa: " << enemigo.defensa << "\n";
+void mostrarEstadisticas(const Estadisticas& jugador, const Estadisticas& enemigo, int nivel){
+    cout << "Jugador - Vida: " << jugador.vida << ", Ataque: " << jugador.ataque << ", Defensa: " << jugador.defensa <<", Nivel: "<< nivel <<endl;
+    cout << "Enemigo - Vida: " << enemigo.vida << ", Ataque: " << enemigo.ataque << ", Defensa: " << enemigo.defensa <<endl;
 }
 // Función para calcular el daño
-int calcularDanio(int ataque, int defensa);
-int calcularDanio(int ataque, int defensa) {
-    int danio = ataque - defensa;
-    return (danio > 0) ? danio : 1;  // Siempre hace al menos 1 de daño
+int calcularDanio(int ataque, int defensa){
+    int dano = ataque - defensa;
+    return (dano > 0) ? dano : 1;// operaor ternario evalualr true o false
 }
 // Función para subir de nivel
-void nivelUp(Estadisticas& jugador, int& nivel);
-void nivelUp(Estadisticas& jugador, int& nivel) {
+void nivelUp(Estadisticas& jugador, int& nivel){
     nivel++;
     jugador.vida += 20;
     jugador.ataque += 5;
     jugador.defensa += 2;
-    cout << "¡Subiste al nivel! " << nivel << "¡Tus estadísticas han mejorado.!"<<endl;
+    cout << "¡Subiste al nivel! " << nivel << "¡Tus estadisticas han mejorado.!"<<endl;
 }
