afb6c287f597043a24107e7789bf9ad605e1e866
Williamf40 <63164108+necrocest@users.noreply.github.com>
2024-11-25 00:23:05 -0500

    Update Proyecto Programacion.cpp

--- Proyecto Programacion.cpp
+++ Proyecto Programacion.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
+#include <string>
 using namespace std;
 // Estructura para las estadísticas de un personaje
 struct Estadisticas{
     int vida;
     int ataque;
     int defensa;
-}
+};
 // Función para mostrar estadísticas
-void mostrarEstadisticas(const Estadisticas& jugador, const Estadisticas& enemigo, int nivel){
+void mostrarEstadisticas(const Estadisticas jugador, const Estadisticas enemigo, int nivel){
     cout << "Jugador - Vida: " << jugador.vida << ", Ataque: " << jugador.ataque << ", Defensa: " << jugador.defensa <<", Nivel: "<< nivel <<endl;
     cout << "Enemigo - Vida: " << enemigo.vida << ", Ataque: " << enemigo.ataque << ", Defensa: " << enemigo.defensa <<endl;
 }
@@ -17,10 +18,29 @@ int calcularDanio(int ataque, int defensa){
     return (dano > 0) ? dano : 1;// operaor ternario evalualr true o false
 }
 // Función para subir de nivel
-void nivelUp(Estadisticas& jugador, int& nivel){
+void nivelUp(Estadisticas jugador, int nivel){
     nivel++;
     jugador.vida += 20;
     jugador.ataque += 5;
     jugador.defensa += 2;
     cout << "¡Subiste al nivel! " << nivel << "¡Tus estadisticas han mejorado.!"<<endl;
+}
+int main(){ 
+    Estadisticas jugador == {100, 25, 20};
+    Estadisticas enemigo == { 75, 20, 15};
+    int nivel = 1; 
+    string nombre;
+    cout<<"El jugador encarna a un heroe que recorre un calabozo enfrentandose"<<endl;
+	cout<<"a una serie de enemigos y tomando decisiones estrategicas para sobrevivir,"<<endl;
+	cout<<"dentro de este mundo las preguntas juegan un papel fundamental buscando determinar"<<endl;
+	cout<<"el valor o la decadencia del heroe. Durante el juego el jugador tendra que"<<endl;
+	cout<<"decidir si luchar o huir, y ganara puntos o perdera vida dependiendo de"<<endl; 
+	cout<<"sus decisiones y de los resultados de los enfrentamientos"<<endl;
+    cout<<"ingrese el nombre de su heroe"<<endl;
+    cin>>nombre;
+	cout <<"Bienvenido a La Senda del Hroe"<<endl;
+    cout <<"Tu objetivo es alcanzar el nivel 5 derrotando enemigos."<<endl;
+    while(nivel<5){
+
+    }
 }
